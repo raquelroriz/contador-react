6e1c3603bf7e26e63340d9a12398f212b9524b7c
Raquel Roriz <maria.raquel.roriz@gmail.com>
2024-12-13 17:53:02 +0100

    Add gif

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-// para inciar no crome use yarn start
+// para inciar no chrome use yarn start
 
 import { useState } from "react";
 
 export default function App() {
   //    let numero = 10
   const [number, setNumber] = useState(0);
+
 
   function increase() {
     setNumber(number + 1);
@@ -14,13 +15,26 @@ export default function App() {
     setNumber(number - 1);
   }
 
+  
   return (
-    <section>
+<div style={{
+    display: 'flex',
+    justifyContent: 'center', // Centraliza horizontalmente
+    alignItems: 'center',    // Centraliza verticalmente
+    height: '100vh',         // Ocupa toda a altura da tela
+}}>
+  <div style={{
+      width: '200px',
+      border: '2px solid black', // Espessura, estilo e cor da borda
+      padding: '10px',           // Espaço interno
+  }}>
       <h1>Counter</h1>
       <p>{number}</p>
       <button onClick={decrease}>- </button>
       <button onClick={increase}>+</button>
+  </div>
+</div>
 
-    </section>
+
   );
 }
